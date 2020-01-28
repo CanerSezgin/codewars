9e7c6291424161a053d6074ef20d704405f3bf8c
Caner Sezgin <ccanersezgin@gmail.com>
2020-01-28 17:39:48 +0300

    test helper upgraded

--- kata/7kyu/Credit Card Mask/test.js
+++ kata/7kyu/Credit Card Mask/test.js
@@ -1,27 +1,27 @@
-import { TestData, TestCases, getTestDescribe } from "../../../lib/testHelper";
+import { TestData, getTestDescribe } from "../../../lib/testHelper";
 import { maskify } from "./solution";
 
 // Here, you can create test cases
 const example = [
-  new TestData("4556364607935616", "############5616"),
-  new TestData("11111", "#1111"),
-  new TestData("Skippy", "##ippy"),
-  new TestData("1", "1"),
-  new TestData("", "")
+  new TestData(["4556364607935616"], "############5616"),
+  new TestData(["11111"], "#1111"),
+  new TestData(["Skippy"], "##ippy"),
+  new TestData(["1"], "1"),
+  new TestData([""], "")
 ];
 const random = [
-  new TestData("wlon", "wlon"),
-  new TestData("8qszjh", "##szjh"),
-  new TestData("7as8dw8mm", "#####w8mm")
+  new TestData(["wlon"], "wlon"),
+  new TestData(["8qszjh"], "##szjh"),
+  new TestData(["7as8dw8mm"], "#####w8mm")
 ];
 const lessThan4Char = [
-  new TestData("trl", "trl"),
-  new TestData("60", "60"),
-  new TestData("f", "f")
+  new TestData(["trl"], "trl"),
+  new TestData(["60"], "60"),
+  new TestData(["f"], "f")
 ]
 
-getTestDescribe(maskify, new TestCases({
+getTestDescribe(maskify, {
   example,
   random,
   lessThan4Char,
-}));
+});
